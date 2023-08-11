f09a284131c54dc2846d295d2e46700d12f18ce2
onurkaymak <onurkaymak@outlook.com>
2023-08-11 13:48:46 -0700

    Create calculator function, form gathers input from the user.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
+function calculator(event) {
+    event.preventDefault();
+
+    const question1 = document.querySelector("input[name='question-1']:checked");
+    const question2 = document.querySelector("input[name='question-2']:checked");
+    const question3 = document.querySelector("input[name='question-3']:checked");
+    const question4 = document.querySelector("input[name='question-4']:checked");
+    const question5 = document.querySelector("input[name='question-5']:checked");
+
+
+}
+
+
+
+
+
+
+
 window.addEventListener("load", function () {
     const form = document.getElementById("form");
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        console.log('submitted!', event);
-    });
+    form.addEventListener("submit", calculator);
 });
