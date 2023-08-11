8761242c75316b69b636f34fcb33a104b1f6fc34
onurkaymak <onurkaymak@outlook.com>
2023-08-11 14:21:44 -0700

    Add branching logic

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,85 @@ function calculator(event) {
     const question4 = document.querySelector("input[name='question-4']:checked");
     const question5 = document.querySelector("input[name='question-5']:checked");
 
+    let javaScript = 0;
+    let python = 0;
+    let java = 0;
+    let cSharp = 0;
+
+    if (question1.value === 'Web Development') {
+        javaScript += 1;
+    }
+    else if (question1.value === 'Data Science') {
+        python += 1;
+    }
+    else if (question1.value === 'Mobile') {
+        java += 1;
+    }
+    else {
+        cSharp += 1;
+    }
+
+    if (question2.value === 'Small business/Freelance') {
+        javaScript += 1;
+    }
+    else if (question2.value === 'Startups') {
+        python += 1;
+    }
+    else if (question2.value === 'No Preference') {
+        java += 1;
+    }
+    else {
+        cSharp += 1;
+    }
+
+
+    if (question3.value === 'Yellow') {
+        javaScript += 1;
+    }
+    else if (question3.value === 'Green') {
+        python += 1;
+    }
+    else if (question3.value === 'Orange') {
+        java += 1;
+    }
+    else {
+        cSharp += 1;
+    }
+
+
+
+    if (question4.value === 'Wolf') {
+        javaScript += 1;
+    }
+    else if (question4.value === 'Turtle') {
+        python += 1;
+    }
+    else if (question4.value === 'Puma') {
+        java += 1;
+    }
+    else {
+        cSharp += 1;
+    }
+
+    if (question5.value === "I'm okay to push myself.") {
+        javaScript += 1;
+    }
+    else if (question5.value === 'No challenge please!') {
+        python += 1;
+    }
+    else if (question5.value === 'I drive a Challanger!') {
+        java += 1;
+    }
+    else {
+        cSharp += 1;
+    }
+
+
+    console.log(javaScript);
+    console.log(python);
+    console.log(java);
+    console.log(cSharp);
+
 
 }
 
